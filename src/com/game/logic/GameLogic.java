25f5fa7a3e33437c5b382b11da5e20e9afb7cbff
Yugesh Pandey <27809103+yugeshpandey@users.noreply.github.com>
2021-06-23 10:58:20 -0400

    Added Masked Word generator

---
+++ src/com/game/logic/GameLogic.java
@@ -0,0 +1,12 @@
+package com.game.logic;
+
+public class GameLogic {
+				
+	public GameLogic() {
+		
+	}
+	
+		
+		
+
+}
--- src/com/game/wordGenerator/WordGenerator.java
+++ src/com/game/wordGenerator/WordGenerator.java
@@ -1,41 +1,39 @@
 package com.game.wordGenerator;
 
+
 public class WordGenerator {
+
+	static int numberOfMaskedWords = 2;//number of words that are masked
 	
-		
+	//generate a random number between variables "max" and "min".
+	public int randomNumberGenerator(int max, int min) {
+				
+				int range = max - min + 1;
+				int result = (int)(Math.random() * range) + min;
+				
+				return result;
+									
+	}
+	  
 	public String generateRandomWord(String[] wordArray) {
 		int rand = randomNumberGenerator(wordArray.length-1, 0); //random number between 0 and number of items in array
 		
 		return wordArray[rand];
 	}
 	
-	//generate a random number between variables "max" and "min".
-	public int randomNumberGenerator(int max, int min) {
+
 		
-		int range = max - min + 1;
-		int result = (int)(Math.random() * range) + min;
+	public String generateMaskedWord(String originalWord) {
+		String maskedWord = originalWord;
 		
-		return result;
-		
-					
-	}
-	
-	public String generateMaskedWord(String randomWord) {
-		int max = randomWord.length()-1;
-		int numberOfHiddenWords = 2;
-		
-		String maskedWord = randomWord;
-		
-		for (int i = 0; i < numberOfHiddenWords; i++) {
-			
-			int randomNumber = randomNumberGenerator(max,0);
-			char charToMask = randomWord.charAt(randomNumber);
-			
-			maskedWord = maskedWord.replace(charToMask, '_');
+		for (int i = 0; i< numberOfMaskedWords; i++) {
+			int randomArrayIndex = randomNumberGenerator(originalWord.length()-1, 0);
+			char randomChar = maskedWord.charAt(randomArrayIndex);
+			maskedWord = maskedWord.replace(randomChar, '_');
 		}
-				
-		
 		return maskedWord;
 	}
+	
+	
 
 }
