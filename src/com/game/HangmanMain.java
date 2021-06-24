a7e4252c50b657dc8b66130e84893dc41cc64f68
Yugesh Pandey <27809103+yugeshpandey@users.noreply.github.com>
2021-06-23 20:53:39 -0400

    Array containing index of matching letters from input

--- src/com/game/HangmanMain.java
+++ src/com/game/HangmanMain.java
@@ -11,41 +11,40 @@ import com.game.wordGenerator.WordGenerator;
 public class HangmanMain {
 
 	public static void main(String[] args) {
-		
+
 		Home home = new Home();
-		InputHelper inputHelper = new InputHelper();
+		//InputHelper inputHelper = new InputHelper();
 		WordGenerator newWord = new WordGenerator();
 		GameLogic gameLogic = new GameLogic();
-		String[] chosenTopic = TopicList.getTopic(1); //Array of chosen list of words
-			
-		String randomWord= newWord.generateRandomWord(chosenTopic);
+		String[] chosenTopic = TopicList.getTopic(1); // Array of chosen list of words
+
+		String randomWord = newWord.generateRandomWord(chosenTopic);
 		String maskedWord = newWord.generateMaskedWord(randomWord);
-		
-		//Welcome screen
+
+		// Welcome screen
 		home.welcome();
 		home.printStickFigure();
 		System.out.println("\n");
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Press Y to begin:");
 		String startResponse = keyboard.nextLine();
-						
-		if(startResponse.equals("Y") || startResponse.equals("y")) {
-						
+
+		if (startResponse.equals("Y") || startResponse.equals("y")) {
+
 			System.out.println(maskedWord);
 			String userInput = gameLogic.promptQuestion();
-						
-			boolean isMatch; 
+
+			boolean isMatch;
 			isMatch = gameLogic.isWordMatch(randomWord, userInput);
-			
-			System.out.println(isMatch);
-								 
-			
-			
-			
+
+			if (isMatch) {
+				String unmaskedWord = gameLogic.unmaskWord(randomWord, userInput, maskedWord);
+				System.out.println(unmaskedWord);
+			}
+
 		} else {
 			System.out.println("Exiting Game");
 		}
-		
 
 	}
 
