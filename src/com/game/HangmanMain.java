bf8e29e0c64bd23aea4cca2d587f31269da2fdca
Yugesh Pandey <27809103+yugeshpandey@users.noreply.github.com>
2021-06-25 11:12:51 -0400

    Fixed main method

--- src/com/game/HangmanMain.java
+++ src/com/game/HangmanMain.java
@@ -2,57 +2,23 @@ package com.game;
 
 import com.game.home.Home;
 import com.game.logic.GameLogic;
-import com.game.wordGenerator.TopicList;
-import com.game.wordGenerator.WordGenerator;
 
 public class HangmanMain {
 
 	public static void main(String[] args) {
 
 		Home home = new Home();
-		// InputHelper inputHelper = new InputHelper();
-		WordGenerator newWord = new WordGenerator();
-		GameLogic gameLogic = new GameLogic();
-		String[] chosenTopic = TopicList.getTopic(1); // Array of chosen list of words
-
-		String randomWord = newWord.generateRandomWord(chosenTopic);
-		String maskedWord = newWord.generateMaskedWord(randomWord);
-		int livesLeft = 7;
-		boolean isMatch;
-
 		// Welcome screen
 		home.welcome();
-		// home.printStickFigure();
-		
-		String startResponse = gameLogic.promptQuestion("Press Y to start: ");
-				
-		if (startResponse.equals("Y") || startResponse.equals("y")) {
-			
-			while (livesLeft != 0) {
-				System.out.println(maskedWord);
-				char userInput = gameLogic.promptQuestionChar("Type your guess: ");
-											
-				isMatch = gameLogic.isWordMatch(randomWord, userInput);
-				
-				if (isMatch) {
-					String unmaskedWord = gameLogic.unmaskWord(randomWord, userInput, maskedWord);
+		home.printStickFigure();
+		System.out.println("\n");
 
-					System.out.println("Correct!");
-					System.out.println(unmaskedWord);
-					unmaskedWord = maskedWord;
-						 
-				} else {
-					System.out.println("Incorrect!");
-					livesLeft = livesLeft - 1;
-				}
-				
-			}
-		
-
-		} else {
-			System.out.println("Exiting Game");
+		GameLogic gameLogic = new GameLogic();
+		boolean isGameFinish = gameLogic.startGame();
+		while (isGameFinish == true) {
+			GameLogic gameLogicnew = new GameLogic();
+			isGameFinish = gameLogicnew.startGame();
 		}
-
 	}
 
 }
--- src/com/game/logic/GameLogic.java
+++ src/com/game/logic/GameLogic.java
@@ -2,19 +2,72 @@ package com.game.logic;
 
 import java.util.Scanner;
 
+import com.game.wordGenerator.TopicList;
+import com.game.wordGenerator.WordGenerator;
+
 public class GameLogic {
-	
+
 	private Scanner keyboard = new Scanner(System.in);
-	
+	WordGenerator newWord = new WordGenerator();
+
 	public GameLogic() {
-		
+
 	}
 	
-	public boolean isWordMatch (String originalWord, char guessedLetter) {
+	public boolean startGame() {
+		int livesLeft = 7;
+		boolean isMatch;
+		boolean isGameWin = false;
+		String[] chosenTopic = TopicList.getTopic(1); // Array of chosen list of words
+
+		String startResponse = promptQuestion("Press Y to start, type any other key to exit: ");
+
+		if (startResponse.equals("Y") || startResponse.equals("y")) {
+
+			String randomWord = newWord.generateRandomWord(chosenTopic);
+			String maskedWord = newWord.generateMaskedWord(randomWord);
+
+			System.out.println(maskedWord);
+
+			while (livesLeft != 0 && isGameWin != true) {
+
+				char userInput = promptQuestionChar("Type your guess: ");
+
+				isMatch = isWordMatch(randomWord, userInput);
+				maskedWord = unmaskWord(randomWord, userInput, maskedWord);
+
+				if (isMatch) {
+					System.out.println("Correct!");
+					System.out.println(maskedWord);
+
+				} else {
+					System.out.println("Incorrect!");
+					livesLeft = livesLeft - 1;
+				}
+
+				if (maskedWord.equals(randomWord)) {
+					isGameWin = true;
+					System.out.println("Congratulations! You won!");
+					return true;
+				}
+
+			}
+
+			if (livesLeft == 0) {
+				System.out.println("Sorry, you lost!");
+				return true;
+			}
+
+		} else {
+			System.out.println("Exiting Game");
+		}
+		return false;
+	}
+
+	public boolean isWordMatch(String originalWord, char guessedLetter) {
 		char[] originalWordArray = originalWord.toCharArray();
 		char guessedChar = Character.toUpperCase(guessedLetter);
-		
-		
+
 		for (int i = 0; i < originalWordArray.length; i++) {
 			if (guessedChar == originalWordArray[i]) {
 				return true;
@@ -23,45 +76,41 @@ public class GameLogic {
 
 		return false;
 	}
-	
+
 	public String promptQuestion(String customPrompt) {
-		
-		
+
 		System.out.println(customPrompt);
 		String userInput = keyboard.nextLine();
-						
+
 		return userInput;
 	}
-	
+
 	public char promptQuestionChar(String customPrompt) {
-		
+
 		System.out.println(customPrompt);
 		char inputChar = keyboard.next().charAt(0);
 		return inputChar;
 	}
-	
+
 	// This method unmasks the masked word using the correct user input
 	public String unmaskWord(String originalWords, char rightInput, String maskedWord) {
 
 		char[] originalWordArray = originalWords.toCharArray();
 		char[] maskedWordArray = maskedWord.toCharArray();
 		char rightInputChar = Character.toUpperCase(rightInput);
-		
+
 		for (int i = 0; i < originalWordArray.length; i++) {
-			
-			if (originalWordArray[i] == Character.toLowerCase(rightInputChar) 
+
+			if (originalWordArray[i] == Character.toLowerCase(rightInputChar)
 					|| originalWordArray[i] == Character.toUpperCase(rightInputChar)) {
-				
+
 				maskedWordArray[i] = Character.toUpperCase(rightInputChar);
-				
+
 			}
 		}
-		
 
 		return String.valueOf(maskedWordArray);
-				
-	
+
 	}
-		
 
 }
